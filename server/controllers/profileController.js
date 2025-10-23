31a2289df43c750ceea2c16581fe6fd66ba2348c
Shubham1823L <shubhuisbetter@gmail.com>
2025-10-23 08:06:52 +0530

    update logic bug fixed

--- server/controllers/profileController.js
+++ server/controllers/profileController.js
@@ -8,11 +8,19 @@ export const getProfileData = (req, res) => {
 export const updateProfileData = async (req, res) => {
     const user = req.user
     const _id = user._id
-    const updates = req.body
-    const updated = {$set:{}}
+
+    const allowedUpdates = ['bio', 'avatar', 'username']
+    const updates = {}
+    for (const element of allowedUpdates) {
+        if (req.body[element] != undefined) {
+            // updates = { ...updates, [element]: req.body[element] }
+            updates[element] = req.body[element]
+        }
+    }
+    const updated = { $set: {} }
     for (const key in updates) {
         updated.$set[key] = updates[key]
     }
-    await User.updateOne({_id},updated)
-    res.status(200).json({message:"Profile Updated"})
+    await User.updateOne({ _id }, updated)
+    res.status(200).json({ message: "Profile Updated" })
 }
