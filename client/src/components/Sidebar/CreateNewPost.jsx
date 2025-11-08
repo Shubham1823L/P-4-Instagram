644b0f2fa64013c1bfef57e6f896182cf92116f4
Shubham1823L <shubhuisbetter@gmail.com>
2025-11-08 11:05:53 +0530

    before starting searchMenu

---
+++ client/src/components/Sidebar/CreateNewPost.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from 'react'
+import styles from './sidebar.module.css'
+import { publishNewPost, uploadFile } from '../../api/posts';
+import { IoClose } from "react-icons/io5";
+import { FaArrowLeftLong } from "react-icons/fa6";
+
+const CreateNewPost = ({createNewPostRef,setMyPosts}) => {
+    const [file, setFile] = useState(null)
+    const [tempURL, setTempURL] = useState(null)
+
+
+    const handleClose = () => {
+        createNewPostRef.current.style.display = "none"
+        setTempURL(null)
+    }
+
+    const handleFileSelect = (e) => {
+        const file = e.target.files[0]
+        const url = URL.createObjectURL(file)
+        setFile(file)
+        setTempURL(url)
+    }
+
+    const handleFileUpload = async (e) => {
+        e.preventDefault()
+        const { status, data } = await uploadFile(file)
+        if (status == 400) return console.log("File was empty, Bad request")
+        if (status == 500) return console.log("Something went wrong on our end")
+        if (status == 200) {
+            const response = await publishNewPost(data)
+            if (response.status == 500) return console.log("Something went wrong on our end")
+            if (response.status == 200) {
+                console.log(response)
+                setMyPosts(prev => [...prev, response.data.post])
+                return
+            }
+        }
+        console.error("SHIT HAPPENED , TOO MUCH ERROR HANDLING, NGL")
+    }
+    return (
+        <div ref={createNewPostRef} className={styles.createNewPost}>
+            <IoClose onClick={handleClose} className={styles.closeIcon} size={32} />
+            <div className={styles.dialogBoxWrapper}>
+                <form className={styles.dialogBox}>
+                    <div>
+                        {tempURL && <button onClick={(e) => {
+                            e.preventDefault()
+                            setTempURL(null)
+                        }} className={styles.backBtn}><FaArrowLeftLong /></button>}
+                        <p>Create New Post</p>
+                        {tempURL && <button onClick={handleFileUpload} className={styles.uploadFile}>Share</button>}
+                    </div>
+
+                    {tempURL ? <img className={styles.previewContent} src={tempURL} alt="userSelectedContentPreview" /> : <>
+                        <section>
+                            <img src="www.instagram.com/image&video.svg" alt="image&videoIcon" />
+                            <p>Drag photos and videos here</p>
+                            <div className={styles.selectFile}>
+                                <label htmlFor="fileUpload">Select from Computer</label>
+                                <input onChange={handleFileSelect} id='fileUpload' type="file" accept='image/* video/*' />
+                            </div>
+                        </section>
+                    </>
+                    }
+
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default CreateNewPost
--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -8,115 +8,69 @@ import { PiPaperPlaneTiltBold } from "react-icons/pi";
 import { FaRegHeart } from "react-icons/fa";
 import { FiPlus } from "react-icons/fi";
 import { BsPersonCircle } from "react-icons/bs";
-import { IoClose } from "react-icons/io5";
-import { FaArrowLeftLong } from "react-icons/fa6";
-import { publishNewPost, uploadFile } from '../../api/posts';
+
+import CreateNewPost from './CreateNewPost';
 
 const Sidebar = ({ setMyPosts, createNewPostRef, showCreateNewPostDialog }) => {
-    const [file, setFile] = useState(null)
-    const [tempURL, setTempURL] = useState(null)
-
-
-    const handleClose = () => {
-        createNewPostRef.current.style.display = "none"
-        setTempURL(null)
-    }
-
-    const handleFileSelect = (e) => {
-        const file = e.target.files[0]
-        const url = URL.createObjectURL(file)
-        setFile(file)
-        setTempURL(url)
-    }
-
-    const handleFileUpload = async (e) => {
-        e.preventDefault()
-        const { status, data } = await uploadFile(file)
-        if (status == 400) return console.log("File was empty, Bad request")
-        if (status == 500) return console.log("Something went wrong on our end")
-        if (status == 200) {
-            const response = await publishNewPost(data)
-            if (response.status == 500) return console.log("Something went wrong on our end")
-            if (response.status == 200) {
-                console.log(response)
-                setMyPosts(prev => [...prev, response.data.post])
-                return
-            }
-        }
-        console.error("SHIT HAPPENED , TOO MUCH ERROR HANDLING, NGL")
-    }
 
     return (
         <div className={styles.sidebarWrapper}>
 
+            <CreateNewPost setMyPosts={setMyPosts} createNewPostRef={createNewPostRef} showCreateNewPostDialog={showCreateNewPostDialog} />
 
-            <div ref={createNewPostRef} className={styles.createNewPost}>
-                <IoClose onClick={handleClose} className={styles.closeIcon} size={32} />
-                <div className={styles.dialogBoxWrapper}>
-                    <form className={styles.dialogBox}>
-                        <div>
-                            {tempURL && <button onClick={(e) => {
-                                e.preventDefault()
-                                setTempURL(null)
-                            }} className={styles.backBtn}><FaArrowLeftLong /></button>}
-                            <p>Create New Post</p>
-                            {tempURL && <button onClick={handleFileUpload} className={styles.uploadFile}>Share</button>}
-                        </div>
 
-                        {tempURL ? <img className={styles.previewContent} src={tempURL} alt="userSelectedContentPreview" /> : <>
-                            <section>
-                                <img src="www.instagram.com/image&video.svg" alt="image&videoIcon" />
-                                <p>Drag photos and videos here</p>
-                                <div className={styles.selectFile}>
-                                    <label htmlFor="fileUpload">Select from Computer</label>
-                                    <input onChange={handleFileSelect} id='fileUpload' type="file" accept='image/* video/*' />
-                                </div>
-                            </section>
-                        </>
-                        }
-
-                    </form>
-                </div>
+            {/* here */}
+            <div className={styles.searchMenuWrapper}>
+                
             </div>
 
 
 
 
             <div className={styles.sidebar}>
-                <div>
-                    <NavLink to={"/"}>
-                        <div><img src="instagram-wordmark.svg" alt="instaLogo" className={styles.instaLogo} /></div>
-                    </NavLink>
-                </div>
+
+                <NavLink to={"/"} className={styles.logoWrapper}>
+                    <img src="instagram-wordmark.svg" alt="instaWordmark" className={styles.instaWordmark} />
+                    <img src="01 Static Glyph/02 White Glyph/Instagram_Glyph_White.svg" alt="instaLogo" className={styles.instaLogo} />
+                </NavLink>
+
                 <ul className={styles.list}>
                     <li className={styles.listItem}>
                         <SidebarNavLink to={"/"}>
                             <div>
                                 <GoHomeFill size={28} />
                             </div>
-                            <span>Home</span>
+                            <span className={styles.listItemText}>Home</span>
                         </SidebarNavLink>
                     </li>
+
+                    {/* SearchBtn */}
+
+
                     <li className={styles.listItem}>
-                        <div>
+                        <button className={styles.searchBtn}>
                             <div>
                                 <CiSearch strokeWidth={.6} size={28} />
                             </div>
-                            <span>Search</span>
-                        </div>
+                            <span className={styles.listItemText}>Search</span>
+                        </button>
                     </li>
+
+
+
+
                     <li className={styles.listItem}>
                         <SidebarNavLink to={"/"}>
                             <div>
                                 <MdOutlineExplore size={28} />
                             </div>
-                            <span>Explore</span>
+                            <span className={styles.listItemText}>Explore</span>
                         </SidebarNavLink>
                     </li>
                     <li className={styles.listItem}>
                         <SidebarNavLink to={"/"}>
                             <div><img src="www.instagram.com/reels.svg" alt="reelsIcon" /></div>
-                            Reels
+                            <span className={styles.listItemText}>Reels</span>
                         </SidebarNavLink>
                     </li>
                     <li className={styles.listItem}>
@@ -124,7 +78,7 @@ const Sidebar = ({ setMyPosts, createNewPostRef, showCreateNewPostDialog }) => {
                             <div>
                                 <PiPaperPlaneTiltBold size={26} />
                             </div>
-                            Messages
+                            <span className={styles.listItemText}>Messages</span>
                         </SidebarNavLink>
                     </li>
                     <li className={styles.listItem}>
@@ -132,7 +86,7 @@ const Sidebar = ({ setMyPosts, createNewPostRef, showCreateNewPostDialog }) => {
                             <div>
                                 <FaRegHeart size={24} />
                             </div>
-                            Notifications
+                            <span className={styles.listItemText}>Notifications</span>
                         </div>
                     </li>
                     <li className={styles.listItem}>
@@ -140,7 +94,7 @@ const Sidebar = ({ setMyPosts, createNewPostRef, showCreateNewPostDialog }) => {
                             <div>
                                 <FiPlus size={28} />
                             </div>
-                            Create
+                            <span className={styles.listItemText}>Create</span>
                         </button>
                     </li>
                     <li className={styles.listItem}>
@@ -148,7 +102,7 @@ const Sidebar = ({ setMyPosts, createNewPostRef, showCreateNewPostDialog }) => {
                             <div>
                                 <BsPersonCircle size={28} />
                             </div>
-                            Profile
+                            <span className={styles.listItemText}>Profile</span>
                         </SidebarNavLink>
                     </li>
                 </ul>
